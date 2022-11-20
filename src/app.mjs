ea46b6e2dee4b2cc5164426cc3541961c8e9d020
Annie00Li <cl5166@nyu.edu>
2022-11-20 16:22:05 -0500

    search result

--- src/app.mjs
+++ src/app.mjs
@@ -80,10 +80,10 @@ app.post('/search', (req, res) => {
 app.get('/search_result', (req, res) => {
   const Scholar = mongoose.model('Scholar');
 
-   Scholar.find({input},(err, scholars) => {
-    res.render('search-result', {scholars: scholars});
+   Scholar.find({}).populate(input).exec((err, scholars) => {
+    res.render('search-result', {home: false, scholars: scholars});
   });
-})
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`);});
