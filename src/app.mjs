96ba9f16e8cd979598bd6e285e195fbd43fc6c93
Annie00Li <cl5166@nyu.edu>
2022-11-20 16:59:48 -0500

    search result

--- src/app.mjs
+++ src/app.mjs
@@ -80,7 +80,7 @@ app.post('/search', (req, res) => {
 app.get('/search_result', (req, res) => {
   const Scholar = mongoose.model('Scholar');
 
-   Scholar.find({input},(scholars) => {
+   Scholar.find({'person'},(scholars) => {
     res.render('search-result', {scholars: scholars});
   });
 })
